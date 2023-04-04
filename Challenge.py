418167fe6df071447ba5d4a85ce064f084bb9821
João Vitor Cunha Assis <121521197+JJCunhaAssis@users.noreply.github.com>
2023-04-04 20:30:21 -0300

    Update Challenge.py
    Challenge python finalizado.

--- Challenge.py
+++ Challenge.py
@@ -1,3 +1,12 @@
+import threading
+
+
+#Função que define o tempo de interação do usuario. Caso ele não interaja, o programa fechará automaticamente.
+def timeout():
+    print('\n\nOperação encerrada devido falta de interação. ')
+    exit()
+
+
 while True:
     print('-=' * 45)
     print('        Olá! Seja bem-vindo ao Assistente de Modal da Porto!            ')
@@ -11,7 +20,12 @@ while True:
     print('5 - Sair')
 
     while True:
+        tempo = threading.Timer(10.00, timeout)
+        tempo.start()
         escolha_usuario = input("Escolha sua opção: ")
+
+        tempo.cancel()
+
         try:
             escolha_usuario = int(escolha_usuario)
             if escolha_usuario not in [1, 2, 3, 4, 5]:
@@ -22,6 +36,8 @@ while True:
             print("Opção inválida. Digite apenas números de 1 a 5.")
 
     if escolha_usuario == 5:
+        print('Operação encerrada pelo usuário')
+
         break
 
     if escolha_usuario == 1:
@@ -38,12 +54,70 @@ while True:
                escolha_eletrico2 = input(('Seu veículo possui carga? [S/N] '))
                if escolha_eletrico2 == 'S':
                    carga_eletrico = float(input('Qual seria o peso aproximadamente?  (Digite com pontos, por exemplo: 23.5)\n'))
-
-
-
-
-
-
+               print('Informações recebidas!')
+               exit();
+            elif escolha_eletrico2 == 'N':
+                print('Informações recebidas')
+                exit();
+    if escolha_usuario == 2:
+        while True:
+            print('Qual seria o problema?  ')
+            print('1 - Motor')
+            print('2 - Transmissão ou freios')
+            print('3 - Suspensão ou eixos')
+            print('4 - Direção')
+            print('5 - Carga tombada')
+            print('6 - Voltar ao menu anterior')
+            escolha_mecanico = input('Escolha sua opção: ')
+            if escolha_mecanico == '6':
+                break
+            elif escolha_mecanico in ['1', '2', '3', '4']:
+                escolha_mecanico2 = input('Seu veículo possui carga? [S/N] ')
+                if escolha_mecanico2 == 'S':
+                    carga_mecanico = float(input('Qual seria o peso aproximadamente?  (Digite com pontos, por exemplo: 23.5)\n'))
+                print('Informações recebidas!')
+                exit();
+            elif escolha_mecanico2 == 'N':
+                print('Informações recebidas')
+                exit();
+    if escolha_usuario == 3:
+        while True:
+            print('Qual seria o problema?  ')
+            print('1 - Falta de Combustível')
+            print('2 - Combustível adulterado')
+            print('3 - Pneu furado')
+            print('4 - Voltar ao menu anterior')
+            escolha_comb = input('Escolha sua opção: ')
+            if escolha_comb == '4':
+                break
+            elif escolha_comb in ['1', '2', '3']:
+                escolha_comb2 = input('Seu veículo possui carga? [S/N] ')
+                if escolha_comb2 == 'S':
+                    carga_comb = float(input('Qual seria o peso aproximadamente?  (Digite com pontos, por exemplo: 23.5)\n'))
+                print('Informações recebidas!')
+                exit();
+            elif escolha_comb2 == 'N':
+                print('Informações recebidas!')
+                exit();
+    if escolha_usuario == 4:
+        while True:
+            print('Qual seria o problema? ')
+            print('1 - Capotamento')
+            print('2 - Batida frontal')
+            print('3 - Veículo atingido')
+            print('4 - Voltar ao menu anterior')
+            escolha_acidente = input('Escolha sua opção: ')
+            if escolha_acidente == '4':
+                break
+            elif escolha_acidente in ['1', '2', '3']:
+                escolha_acidente2 = input('Seu veículo possui carga? [S/N] ')
+                if escolha_acidente2 == 'S':
+                    carga_acidente = float(input('Qual seria o peso aproximadamente?  (Digite com pontos, por exemplo: 25.3)\n'))
+                print('Informações recebidas!')
+                exit();
+            elif escolha_acidente2 == 'N':
+                print('Informações recebidas!')
+                exit();
 
 
 
